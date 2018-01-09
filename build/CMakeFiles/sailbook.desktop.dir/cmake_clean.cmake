f4aa7da020f4d6c160372c066170d1c34a50c308
Rudi Timmermans <rudi.timmer@gmx.us>
2018-01-09 15:09:31 +0100

    Update build files

--- build/CMakeFiles/sailbook.desktop.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sailbook.desktop"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/sailbook.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
